a40558279053acff146fec499e2aab18eee32b38
manuelazotavio <o.manuela@aluno.ifsp.edu.br>
2025-03-17 14:49:57 -0300

    feat: add validation in products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+  validates :name, :presence => true,  :length => { :minimum => 3 }
+  validates :price, :presence => true, comparison: { greater_than: 0 }
+  validates :stock, :presence => true, comparison: { greater_than: 0 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_17_172105) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_17_172938) do
+  create_table "categories", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.decimal "price"
