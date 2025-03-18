66623288c18cbe30b2964b6053cbe47f4da78202
manuelazotavio <o.manuela@aluno.ifsp.edu.br>
2025-03-18 13:55:25 -0300

    feat: add class category

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,5 @@ class Product < ApplicationRecord
   validates :name, :presence => true,  :length => { :minimum => 3 }
   validates :price, :presence => true, comparison: { greater_than: 0 }
   validates :stock, :presence => true, comparison: { greater_than: 0 }
+  belongs_to :category
 end
